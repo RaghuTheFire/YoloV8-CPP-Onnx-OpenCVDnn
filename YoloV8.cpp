85502b88030fb055db07abddef53e22e75f89744
Raghunath Nagaraj <67452178+raghunihal@users.noreply.github.com>
2024-05-02 11:57:10 +0530

    Add files via upload

---
+++ YoloV8.cpp
@@ -0,0 +1,229 @@
+#include <fstream>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+using namespace cv::dnn;
+/******************************************************************************************************************/
+const float INPUT_WIDTH = 640.0;
+const float INPUT_HEIGHT = 480.0;
+const float SCORE_THRESHOLD = 0.45;
+const float NMS_THRESHOLD = 0.5;
+const float CONFIDENCE_THRESHOLD = 0.25;
+
+// Text parameters.
+const float FONT_SCALE = 0.7;
+const int FONT_FACE = FONT_HERSHEY_SIMPLEX;
+const int THICKNESS = 1;
+
+// Colors.
+//const std::vector<cv::Scalar> colors = { cv::Scalar(255, 255, 0), cv::Scalar(0, 255, 0), cv::Scalar(0, 255, 255), cv::Scalar(255, 0, 0) };
+Scalar BLACK = Scalar(0, 0, 0);
+Scalar BLUE = Scalar(255, 178, 50);
+Scalar YELLOW = Scalar(0, 255, 255);
+Scalar RED = Scalar(0, 0, 255);
+/******************************************************************************************************************/
+void draw_label(Mat& input_image, string label, int left, int top)
+{
+    // Display the label at the top of the bounding box.
+    int baseLine;
+    Size label_size = getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS, &baseLine);
+    top = max(top, label_size.height);
+    // Top left corner.
+    Point tlc = Point(left, top);
+    // Bottom right corner.
+    Point brc = Point(left + label_size.width, top + label_size.height + baseLine);
+    // Draw white rectangle.
+    rectangle(input_image, tlc, brc, BLACK, FILLED);
+    // Put the label on the black rectangle.
+    putText(input_image, label, Point(left, top + label_size.height), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS);
+}
+/******************************************************************************************************************/
+std::vector<std::string> load_class_list()
+{
+    std::vector<std::string> class_list;
+    std::ifstream ifs("classes.txt");
+    std::string line;
+    while (getline(ifs, line))
+    {
+        class_list.push_back(line);
+    }
+    return class_list;
+}
+/******************************************************************************************************************/
+void load_net(cv::dnn::Net& net, bool is_cuda)
+{
+    auto result = cv::dnn::readNet("yolov8s.onnx");
+
+    if (is_cuda)
+    {
+        std::cout << "Running on GPU using CUDA\n";
+        result.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
+        result.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA_FP16);
+    }
+    else
+    {
+        std::cout << "Running on CPU\n";
+        result.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
+        result.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
+    }
+    net = result;
+}
+
+/******************************************************************************************************************/
+vector<Mat> pre_process(Mat& input_image, Net& net)
+{
+    //Convert to blob.
+    Mat blob;
+    blobFromImage(input_image, blob, 1. / 255., Size(INPUT_WIDTH, INPUT_HEIGHT), Scalar(), true, false);
+    net.setInput(blob);
+    vector<Mat> outputs;
+    if (!blob.empty()) 
+	{
+        net.forward(outputs, net.getUnconnectedOutLayersNames());
+    }
+
+    return outputs;
+}
+/******************************************************************************************************************/
+Mat post_process(Mat& input_image, vector<Mat>& outputs, const vector<string>& class_name)
+{
+    // Initialize vectors to hold respective outputs while unwrapping     detections.
+    vector<int> class_ids;
+    vector<float> confidences;
+    vector<Rect> boxes;
+
+    int rows = outputs[0].size[2];
+    int dimensions = outputs[0].size[1];
+
+    outputs[0] = outputs[0].reshape(1, dimensions);
+    cv::transpose(outputs[0], outputs[0]);
+
+    float* data = (float*)outputs[0].data;
+
+    // Resizing factor.
+    float x_factor = input_image.cols / INPUT_WIDTH;
+    float y_factor = input_image.rows / INPUT_HEIGHT;
+
+    // Iterate through  detections.
+    //cout << "num detections  : " << rows << " " << dimensions << endl;
+    for (int i = 0; i < rows; ++i)
+    {
+        float* classes_scores = data + 4;
+
+        cv::Mat scores(1, class_name.size(), CV_32FC1, classes_scores);
+        cv::Point class_id;
+        double maxClassScore;
+
+        minMaxLoc(scores, 0, &maxClassScore, 0, &class_id);
+
+        if (maxClassScore > SCORE_THRESHOLD)
+        {
+            confidences.push_back(maxClassScore);
+            class_ids.push_back(class_id.x);
+
+            float x = data[0];
+            float y = data[1];
+            float w = data[2];
+            float h = data[3];
+
+            int left = int((x - 0.5 * w) * x_factor);
+            int top = int((y - 0.5 * h) * y_factor);
+
+            int width = int(w * x_factor);
+            int height = int(h * y_factor);
+
+            boxes.push_back(cv::Rect(left, top, width, height));
+        }
+
+        data += dimensions;
+    }
+    // Perform Non-Maximum Suppression and draw predictions.
+    vector<int> indices;
+    NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD, indices);
+    //cout << "num detections finally : "<< indices.size() <<endl;
+    for (int i = 0; i < indices.size(); i++)
+    {
+        int idx = indices[i];
+        Rect box = boxes[idx];
+        int left = box.x;
+        int top = box.y;
+        int width = box.width;
+        int height = box.height;
+        // Draw bounding box.
+        rectangle(input_image, Point(left, top), Point(left + width, top + height), BLUE, 3 * THICKNESS);
+        // Get the label for the class name and its confidence.
+        string label = format("%.2f", confidences[idx]);
+        label = class_name[class_ids[idx]] + ":" + label;
+        // Draw class labels.
+        draw_label(input_image, label, left, top);
+    }
+    return input_image;
+}
+/******************************************************************************************************************/
+int main()
+{
+    // Load class list.
+    vector<string> class_list = load_class_list();;
+    Mat frame;
+    VideoCapture cap("sample.mp4");
+    Net net;
+    bool is_cuda = false;
+    load_net(net, is_cuda);
+    vector<Mat> detections;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    int frame_count = 0;
+    float fps = -1;
+    int total_frames = 0;
+
+
+    while (true) 
+	{
+        cap.read(frame);
+        // Process the image.
+        detections = pre_process(frame, net);
+        //cout << "number of detections : " << detections.size() << endl;
+        Mat img = post_process(frame, detections, class_list);
+        frame_count++;
+        total_frames++;
+        // Put efficiency information.
+        // The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes).
+        vector<double> layersTimes;
+        double freq = getTickFrequency() / 1000;
+        double t = net.getPerfProfile(layersTimes) / freq;
+        string label = format("Inference time : %.2f ms", t);
+        putText(img, label, Point(20, 40), FONT_FACE, FONT_SCALE, RED);
+
+        if (frame_count >= 30)
+        {
+
+            auto end = std::chrono::high_resolution_clock::now();
+            fps = frame_count * 1000.0 / std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+            frame_count = 0;
+            start = std::chrono::high_resolution_clock::now();
+        }
+
+        if (fps > 0)
+        {
+
+            std::ostringstream fps_label;
+            fps_label << std::fixed << std::setprecision(2);
+            fps_label << "FPS: " << fps;
+            std::string fps_label_str = fps_label.str();
+
+            cv::putText(img, fps_label_str.c_str(), cv::Point(10, 25), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
+        }
+        imshow("Output", img);
+
+        char c = (char)waitKey(1);
+        if (c == 27)
+            break;
+    }
+    cap.release();
+    destroyAllWindows();
+    
+    return 0;
+}
+
